d6b73fd41bcc0b003353f1175b87aa149bbe4a74
Masoud <78568883+masoodhesami@users.noreply.github.com>
2023-01-07 01:50:28 +0330

    loading parameter added to useRestaurants state

--- src/hooks/useRestaurants.jsx
+++ src/hooks/useRestaurants.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import axios from "../api/axios";
 
 export default () => {
-    const [results, setResults] = useState([]);
+    const [results, setResults] = useState({ data: [], loading: false });
 
     const searchRestaurants = async () => {
+        setResults({ data: [], loading: true })
         try {
             const response = await axios.get('/api/restaurants');
-            setResults(response.data);
+            setResults({ data: response.data, loading: false });
         }
         catch (error) {
             console.log(error);
